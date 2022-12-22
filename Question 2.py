969152af03294ce1ef9f0412dbd67cf9cd68df6e
printsartajsingh <121234384+printsartajsingh@users.noreply.github.com>
2022-12-22 19:26:28 +0530

    Add files via upload

---
+++ Question 2.py
@@ -0,0 +1,5 @@
+name = "Sartaj Singh"
+SID = "22106054"
+department = " Data Science "
+CGPA = 9.9
+print("Hey,", name, "Here!\nMy SID is", SID, "\nI am from", department, "department and my CGPA is", CGPA)
